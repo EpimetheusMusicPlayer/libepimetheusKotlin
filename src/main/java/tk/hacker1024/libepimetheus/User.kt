685209d7d3dae646109808dcc48ea1ff16efdc2d
Unknown <joshleivenzon@outlook.com>
2018-10-05 08:32:07 +1000

    Add username to User object, fix constructors

--- src/main/java/tk/hacker1024/libepimetheus/User.kt
+++ src/main/java/tk/hacker1024/libepimetheus/User.kt
@@ -8,44 +8,46 @@ import org.json.JSONObject
  * This class holds the authentication token and user information.
  * All other functions in this library that call Pandora APIs will need an instance of this class.
  * The Pandora authentication API is documented [here](https://6xq.net/pandora-apidoc/rest/authentication/).
- *
- * @constructor Signs in to Pandora, and retrieves the authentication token.
- * @property [email] The email address of the user to sign in with.
- * @param [password] The user's password.
- * @property [usePortaller] Whether to use the free Portaller smart DNS service or not, to access
- *                          Pandora outside the US.
- * @param [authToken] An existing authentication token to use. Don't set this unless you need to.
- *                    This paramater exists mainly for the Parcelable implementation.
  */
-class User(val email: String, password: String? = null, var usePortaller: Boolean, authToken: String? = null) : Parcelable {
+class User : Parcelable {
+    internal val usePortaller: Boolean
     internal val authToken: String
+    internal val email: String
+    internal val username: String
 
-    constructor(parcel: Parcel) : this(
-        email = parcel.readString()!!,
-        authToken = parcel.readString()!!,
-        usePortaller = parcel.readByte() != 0.toByte()
-    )
-
-    init {
-        if (email.isEmpty() || password.isNullOrEmpty() && authToken.isNullOrEmpty()) {
+    /**
+     * @constructor Signs in to Pandora, and retrieves the authentication token.
+     * @property [email] The email address of the user to sign in with.
+     * @param [password] The user's password.
+     * @property [usePortaller] Whether to use the free Portaller smart DNS service or not, to access
+     *                          Pandora outside the US.
+     */
+    constructor(email: String, password: String, usePortaller: Boolean) {
+        if (email.isEmpty() || password.isEmpty()) {
             throw InvalidLoginException("AUTH_INVALID_USERNAME_PASSWORD", "Invalid username and/or password")
         }
-        if (authToken == null && password != null) {
-            // Make the API request, and save the authentication token
-            Networking.makeApiRequest(
-                "auth/login",
-                JSONObject()
-                    .put("existingAuthToken", null)
-                    .put("keepLoggedIn", true)
-                    .put("username", email)
-                    .put("password", password),
-                usePortaller = usePortaller
-            ).apply {
-                this@User.authToken = getString("authToken")
-            }
-        } else {
-            this.authToken = authToken!!
+        this.usePortaller = usePortaller
+        this.email = email
+        // Make the API request, and save the authentication token
+        Networking.makeApiRequest(
+            "auth/login",
+            JSONObject()
+                .put("existingAuthToken", null)
+                .put("keepLoggedIn", true)
+                .put("username", email)
+                .put("password", password),
+            usePortaller = usePortaller
+        ).apply {
+            this@User.authToken = getString("authToken")
+            username = getString("webname")
         }
+    }
+
+    private constructor(parcel: Parcel) {
+        usePortaller = parcel.readByte() != 0.toByte()
+        authToken = parcel.readString()!!
+        email = parcel.readString()!!
+        username = parcel.readString()!!
     }
 
     override fun equals(other: Any?) =
@@ -56,9 +58,10 @@ class User(val email: String, password: String? = null, var usePortaller: Boolea
     override fun hashCode() = 31 * email.hashCode() + authToken.hashCode()
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(email)
-        parcel.writeString(authToken)
         parcel.writeByte(if (usePortaller) 1 else 0)
+        parcel.writeString(authToken)
+        parcel.writeString(email)
+        parcel.writeString(username)
     }
 
     override fun describeContents(): Int {
